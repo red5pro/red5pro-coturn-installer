84d45346bfaeb83df086d9484a7315b889178e76
Mark Pace <pace@illuminati.org>
2022-04-13 11:00:38 -0700

    updating for 10.3

--- r5coturninstall.sh
+++ r5coturninstall.sh
@@ -57,7 +57,11 @@ if [ -d /usr/local/red5pro/webapps/live/script ]; then
   sed -i 's/var iceServers.*/var iceServers = [{ urls: "stun:'"$FQDN"':3478" }]/g' /usr/local/red5pro/webapps/live/script/r5pro-publisher-failover.js
   sed -i 's/var iceServers.*/var iceServers = [{ urls: "stun:'"$FQDN"':3478" }]/g' /usr/local/red5pro/webapps/live/script/r5pro-subscriber-failover.js
   sed -i 's/var iceServers.*/var iceServers = [{ urls: "stun:'"$FQDN"':3478" }]/g' /usr/local/red5pro/webapps/live/script/r5pro-viewer-failover.js
-  systemctl restart red5pro
+  sed -i 's/stun.address.*/stun.address='"$FQDN"':3478/g' /usr/local/red5pro/conf/webrtc.properties
+  # 10.3 added a new setting location
+  if [ -f /usr/local/red5pro/conf/network.properties ]; then
+    sed -i 's/stun.address.*/stun.address='"$FQDN"':3478/g' /usr/local/red5pro/conf/network.properties
+  fi  systemctl restart red5pro
 else
   echo "... red5pro not installed properly webapps/live/script is missing ..."
   exit 3
